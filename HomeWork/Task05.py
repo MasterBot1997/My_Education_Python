be3b3c6ca45576b7cf91e40e1e7f77c9517bec16
Ivan Belyaev <bel3187228@yandex.ru>
2022-12-17 18:19:40 +0300

    Сделана задача 1
    В целом было не сложно, закомментировал один вариант решения с выводом названия дня недели..

---
+++ HomeWork/Task05.py
@@ -0,0 +1,18 @@
+# 5. Напишите программу, которая принимает на вход координаты 
+# двух точек и находит расстояние между ними в 2D пространстве. 
+# https://ru.onlinemschool.com/math/library/analytic_geometry/point_point_length/
+
+
+x1 = int(input("Введите значение X1: "))
+x2 = int(input("Введите значение X2: "))
+y1 = int(input("Введите значение Y1: "))
+y2 = int(input("Введите значение Y2: "))
+
+from math import sqrt
+rangeX = x1 - x2
+rangeY = y1 - y2
+# result = (rangeX * rangeX + rangeY * rangeY) ** 0.5
+result = round(sqrt(rangeX * rangeX + rangeY * rangeY), 2)
+
+# print(f"Расстояние между точками A({x1},{y1}) и В({x2},{y2}) равно {result:.2f}")
+print(f"Расстояние между точками A({x1},{y1}) и В({x2},{y2}) равно {result}")
