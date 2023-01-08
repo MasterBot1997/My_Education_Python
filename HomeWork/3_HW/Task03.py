b2bdd84b81c192484b5ca919e384b8359397da31
Ivan Belyaev <bel3187228@yandex.ru>
2023-01-08 23:10:23 +0300

    Выполнено задание 3

---
+++ HomeWork/3_HW/Task03.py
@@ -0,0 +1,41 @@
+# 3. Напишите программу, которая будет преобразовывать 
+# десятичное число в двоичное.
+# Без использования встроенной функции преобразования, без строк.
+
+# in
+# 88
+# out
+# 1011000
+
+# in
+# 11
+# out
+# 1011
+
+
+def binary_number (num):
+    n = []
+    while num:
+        n.append(num % 2)
+        num = int(num / 2)
+    return n
+
+number = int(input('Введите десятичное число: '))
+my_list = binary_number(number)
+my_list.reverse()
+print(my_list)
+
+# k = 11
+# while k:
+#     if k >= 2:
+#         n = k % 2
+#         k = k - int(k/2)
+#         print(n)
+#     else:
+#         print(k)
+        
+
+
+# k = 11
+# k = int(k/2)
+# print(k)
