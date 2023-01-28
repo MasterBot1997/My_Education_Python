7055b20b0430bff3f8fa8dbb08687f2bb7974be1
Ivan Belyaev <bel3187228@yandex.ru>
2023-01-28 18:03:59 +0300

    задание 2
    - Попробовал реализовать запись в код
    - убрал лишний текст
    - добавил новые комментарии

--- HomeWork/5_HW/Task02.py
+++ HomeWork/5_HW/Task02.py
@@ -5,36 +5,72 @@ from itertools import groupby, starmap
 from os import path
 
 
-def rle_encoding (text="text_words.txt", code_text="text_code_words.txt"):
+def rle_encoding(text="text_words.txt", code_text="text_code_words.txt"):
     if path.exists(text):
-        with open(text) as my_f_1, \
-                open(code_text, "a") as my_f_2:
+        with open(text, "r") as my_f_1, \
+                open(code_text, "w") as my_f_2:
             for i in my_f_1:
-                my_f_2.write("".join([f"{len(list(v))}{ch}" for ch, v in groupby(i)]))
+                my_f_2.write(
+                    "".join([f"{len(list(v))}{ch}" for ch, v in groupby(i)]))
     else:
         print("Такого файла нет в системе!")
 
-def rle_decoding (code_text="text_code_words.txt", decode_text="text_decode_words.txt"):
+# Вариант с (n = 1) вместо (n += i), в данном варианте происходит корректный вывод в консоль
+# def rle_decoding (code_text="text_code_words.txt", decode_text="text_decode_words.txt"):
+#     if path.exists(code_text):
+#         with open(code_text) as my_f_1:
+#             n = ""
+#             for k in my_f_1:
+#                 w_num = []
+#                 for i in k.strip():
+#                     if i.isdigit():
+#                         n = i
+#                     else:
+#                         w_num.append([int(n), i])
+#                         n = ""
+#                 print("".join(starmap(lambda x, y: x * y, w_num)))
+#     else:
+#         print("Такого файла нет в системе!")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Вариант с (n += i), в эом варианте вывод в консоль будет не корректным(текст будет выводиться с неверным количеством символов)
+# Например: превая строка 2а2А1\n вторая 2с2С, "1" которая в кнце строки приплюсуется строчно к следующей и получится 12с вместо 2с.
+
+# def rle_decoding(code_text="text_code_words.txt", decode_text="text_decode_words.txt"):
+#     if path.exists(code_text):
+#         with open(code_text) as my_f_1:
+#             n = ""
+#             for k in my_f_1:
+#                 w_num = []
+#                 for i in k.strip():
+#                     if i.isdigit():
+#                         n += i
+#                     else:
+#                         w_num.append([int(n), i])
+#                         n = ""
+#                 print("".join(starmap(lambda x, y: x * y, w_num)))
+#     else:
+#         print("Такого файла нет в системе!")
+
+# -------------------------------------------------------------------------------------------------------------
+
+# Варинат с записью декодированного текста в файл(записывает только в одну строку все, так и не удалось сделать корректным)
+def rle_decoding(code_text="text_code_words.txt", decode_text="text_decode_words.txt"):
     if path.exists(code_text):
-        with open(code_text) as my_f_1:
-        # with open(code_text) as my_f_1, \
-        #         open(decode_text, "a") as my_f_2:
+        with open(code_text, "r") as my_f_1, \
+                open(decode_text, "w") as my_f_2:
             n = ""
             for k in my_f_1:
                 w_num = []
                 for i in k.strip():
                     if i.isdigit():
-                        # Если написать n += 1 - тогда при декодировании
-                        # будет плюсоваться 1(как строка) к кол-ву символов 
-                        # начала след. строки и тогда декодинг не корректный 
                         n = i
                     else:
                         w_num.append([int(n), i])
                         n = ""
-                print("".join(starmap(lambda x, y: x * y, w_num)))
-                # my_f_2.write("".join(starmap(lambda x, y: x * y, w_num)))
-                # Попробовал сделать запись сразу в файл, но в таком варианте
-                # Все возвращается записью в одну строку
+            my_f_2.writelines("".join(starmap(lambda x, y: x * y, w_num)))
+            print("".join(starmap(lambda x, y: x * y, w_num)))
     else:
         print("Такого файла нет в системе!")
 
