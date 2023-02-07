6b4c69d584e5069f14b58f62d25e3913a84582c2
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-07 17:03:09 +0300

    Update mod_calc.py
    Написан блок с математическими вычислениям для калькулятора

---
+++ HomeWork/7_HW/mod_calc.py
@@ -0,0 +1,22 @@
+def sum(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+def pow(a):
+    return a * 0.5
+
+
+def sqrt(a, b):
+    return a ** b
