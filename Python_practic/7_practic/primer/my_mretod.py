858ed47c5d08bb27cf6832a7697d404c264278c5
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-07 12:43:51 +0300

    Практика по 7 семинара

---
+++ Python_practic/7_practic/primer/my_mretod.py
@@ -0,0 +1,12 @@
+def hi():
+    print("Hi!")
+
+def sim():
+    num = int(input("Enter a num: "))
+    return num ** 5 + 14
+
+
+if __name__ == "__main__":
+    print("i'm a file")
+else:
+    print("i'm a module")
---
+++ Python_practic/7_practic/primer/new_file.py
@@ -0,0 +1,4 @@
+import my_mretod as Li
+
+Li.hi()
+print(Li.sim())
