0bfe701ab0e865da1ee001e9a6317dc817654d1f
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-10 01:47:49 +0300

    Update user_interface.py
    Добавлено меню работы с комплексными числами:
    - Не понял как правильно записать проверку в ecept файл, поэтому она находиться прямо тут

--- HomeWork/7_HW/user_interface.py
+++ HomeWork/7_HW/user_interface.py
@@ -8,13 +8,13 @@ def menu():
     while True:
         num_type = input("\nEnter\n"
                          "1 - rational calc\n"
-                        #  "2 - complex calc\n"
+                         "2 - complex calc\n"
                          "3 - Exit\n")
         match num_type:
             case "1":
                 menu_calc()
-            # case "2":
-            #     menu_calc(num_type)
+            case "2":
+                menu_calc_comp()
             case "3":
                 logging.info("Stop program")
                 print("Thank you for using the calculator, have a nice day!")
@@ -66,7 +66,6 @@ def menu_calc():
                 print(f"{a} * {b} = {res}")
                 logging.info(f"Div: {a} * {b} = {res}")
             case "4":
-                # Дорабатать знаки
                 b = check_number_zero(b)
                 c = division_sign()
                 res = div(a, b, c)
@@ -104,3 +103,80 @@ def division_sign():
             logging.warning("Enter division sign error.")
             print("Error division sign!")
             continue
+
+def menu_calc_comp():
+    logging.info(f"Start menu calc. With complex.")
+    while True:
+        rat = input("\nEnter\n"
+                    "1 - sum\n"
+                    "2 - sub\n"
+                    "3 - mul\n"
+                    "4 - div\n"
+                    "0 - back to menu\n")
+
+        if rat == "0":
+            logging.info('Return menu')
+            print("\nReturn menu")
+            break
+        else:
+            num_1 = input("Enter number 1: ")
+            a = check_data_user(num_1)
+            num_2 = input("Enter number 2: ")
+            b = check_data_user(num_2)
+            if rat == "4":
+                # Не совсем понял как правильно записать функцию проверки 0
+                # в знаминателе для комплексных чисел, поэтому она тут
+                num_3 = input("Enter number 3: ")
+                num_4 = input("Enter number 4: ")
+                if num_3 == "0" and num_4 == "0":
+                    logging.info("Both numbers are 0.")
+                    while True:
+                        press = input("\nОба числа равны 0. Выберете число которое введете заново"
+                                    "\n1 - number 3"
+                                    "\n2 - number 4\n")
+                        match press:
+                            case "1":
+                                num_3 = input("Enter number 3: ")
+                                logging.info(f"Number 3 was changed to {num_3}")
+                                if num_3 != "0":
+                                    c = check_data_user(num_3)
+                                    break
+                            case "2":
+                                num_4 = input("Enter number 4: ")
+                                logging.info(f"Number 3 was changed to {num_4}")
+                                if num_4 != "0":
+                                    c = check_data_user(num_4)
+                                    break
+                            case _:
+                                logging.warning("Main menu, wrong item selected.")
+                                print("Error")
+                                continue
+                else:
+                    c = check_data_user(num_3)
+                    d = check_data_user(num_4)
+            else:
+                num_3 = input("Enter number 3: ")
+                c = check_data_user(num_3)
+                num_4 = input("Enter number 4: ")
+                d = check_data_user(num_4)
+        match rat:
+            case "1":
+                res = sum_comp(a, b, c, d)
+                print(f"({a} + {b}j) + ({c} + {d}j) = {res}")
+                logging.info(f"Sum_complex: ({a} + {b}j) + ({c} + {d}j) = {res}")
+            case "2":
+                res = sub_comp(a, b, c, d)
+                print(f"({a} + {b}j) - ({c} + {d}j) = {res}")
+                logging.info(f"Sub_complex: ({a} + {b}j) - ({c} + {d}j) = {res}")
+            case "3":
+                res = mul_comp(a, b, c, d)
+                print(f"({a} + {b}j) * ({c} + {d}j) = {res}")
+                logging.info(f"Sub_complex: ({a} + {b}j) * ({c} + {d}j) = {res}")
+            case "4":
+                res = div_comp(a, b, c, d)
+                print(f"({a} + {b}j) / ({c} + {d}j) = {res}")
+                logging.info(f"Div_complex: ({a} + {b}j) / ({c} + {d}j) = {res}")
+            case _:
+                logging.warning("Main menu, wrong item selected.")
+                print("Error")
+                continue
