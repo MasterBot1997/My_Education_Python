3f07aeab9d94c119791afe732b69447b6c1a1442
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-10 01:44:18 +0300

    Update mod_calc.py
    Добавлены операции вычислений для комплексных чисел:
    - Сложение
    - Вычитание
    - Умножение
    - Деление

--- HomeWork/7_HW/mod_calc.py
+++ HomeWork/7_HW/mod_calc.py
@@ -1,3 +1,4 @@
+# Все типы операций для рациональных чисел
 def sum(a, b):
     return a + b
 
@@ -25,3 +26,66 @@ def pow(a):
 
 def sqrt(a, b):
     return a ** b
+
+# Всее типы операций для комлексных чисел
+
+def sum_comp(a, b, c, d):
+    res1 = a + c
+    res2 = b + d
+    if res1 == 0:
+        return f"{res2}j"
+    elif res2 == 0:
+        return f"{res1}"
+    elif res1 == 0 and res2 == 0:
+        return 0
+    elif res2 < 0:
+        res2 = res2 * -1
+        return f"{res1} - {res2}j"
+    else:
+        return f"{res1} + {res2}j"
+
+
+def sub_comp(a, b, c, d):
+    res1 = a - c
+    res2 = b - d
+    if res1 == 0:
+        return f"{res2}j"
+    elif res2 == 0:
+        return f"{res1}"
+    elif res1 == 0 and res2 == 0:
+        return 0
+    elif res2 < 0:
+        res2 = res2 * -1
+        return f"{res1} - {res2}j"
+    else:
+        return f"{res1} + {res2}j"
+
+
+def mul_comp(a, b, c, d):
+    res1 = a*c - b*d
+    res2 = b*c + a*d
+    if res1 == 0:
+        return f"{res2}j"
+    elif res2 == 0:
+        return f"{res1}"
+    elif res1 == 0 and res2 == 0:
+        return 0
+    elif res2 < 0:
+        res2 = res2 * -1
+        return f"{res1} - {res2}j"
+    else:
+        return f"{res1} + {res2}j"
+
+
+def div_comp(a, b, c, d):
+    res1 = round(((a*c+b*d)/(c**2 + d**2)), 4)
+    res2 = round(((b*c-a*d)/(c**2 + d**2)), 4)
+    if res1 == 0:
+        return f"{res2}j"
+    elif res2 == 0:
+        return f"{res1} + j"
+    elif res2 < 0:
+        res2 = res2 * -1
+        return f"{res1} - {res2}j"
+    else:
+        return f"{res1} + {res2}j"
