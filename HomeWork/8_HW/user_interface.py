a164f94cb2eb67308c5f11c5bea4bce39c401836
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-13 18:10:52 +0300

    Update user_interface.py
    Добавлено меню для поиска информации по фильтру данных

--- HomeWork/8_HW/user_interface.py
+++ HomeWork/8_HW/user_interface.py
@@ -30,8 +30,7 @@ def menu():
                 print_file()
             case "2":
                 # Пользователь вводит id что бы проверить какая есть информация по этому id
-                key_id = input("\nВведите номе id сотрудника: ")
-                prin_dict(key_id)
+                prin_dict_menu()
             case "3":
                 new_entry(id_last)
             case "4":
@@ -50,3 +49,58 @@ def menu():
                 continue
 
 
+def prin_dict_menu():
+    while True:
+        name_key = input("Ввдеите по какому параетру вы хотите вывести иформацию: \n"
+                        "1 - id\n"
+                        "2 - Name\n"
+                        "3 - Last_name\n"
+                        "4 - Start_work\n"
+                        "5 - job_title\n"
+                        "6 - number_phone\n"
+                        "0 - Вернуться в меню\n")
+        match name_key:
+            case '1':
+                key_user = input("Введите id сотрудника: ")
+                print()
+                filter_user = "id"
+                prin_dict(filter_user, key_user)
+                break
+            case '2':
+                key_user = input("Введите Имя сотрудника: ")
+                print()
+                filter_user = "Name"
+                prin_dict(filter_user, key_user)
+                break
+            case '3':
+                key_user = input("Введите Фамилию сотрудника: ")
+                print()
+                filter_user = "Last_name"
+                prin_dict(filter_user, key_user)
+                break
+            case '4':
+                key_user = input("Введите дату начала работы сотрудника: ")
+                print()
+                filter_user = "Start_work"
+                prin_dict(filter_user, key_user)
+                break
+            case '5':
+                key_user = input("Введите должность сотрудника: ")
+                print()
+                filter_user = "job_title"
+                prin_dict(filter_user, key_user)
+                break
+            case '6':
+                key_user = input("Введите номер телефона сотрудника: ")
+                print()
+                filter_user = "number_phone"
+                prin_dict(filter_user, key_user)
+                break
+            case '0':
+                logging.info("Возвращение в меню")
+                print("\nВы возвращаетесь в меню\n")
+                menu()
+            case _:
+                logging.warning("Пользователь ввел некоректное значение!")
+                print("Такого варанта нет, попробуйте еще раз!")
+                continue
