a06106f8ffbec02df4a85d6d4f7f85f7ef720ecc
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-16 19:22:48 +0300

    Update logg.py
    Удалил один логер из за его бешенной записи, решил его пока что не добавлять

--- HomeWork/9_HW/logg.py
+++ HomeWork/9_HW/logg.py
@@ -6,10 +6,3 @@ def log_bro(update: Update, context: ContextTypes.DEFAULT_TYPE):
     file = open('ds.csv', 'a')
     file.write(f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}''\n')
     file.close()
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename="my_log.log",
-    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
-    datefmt='%d-%m-%Y %H:%M:%S',
-)
