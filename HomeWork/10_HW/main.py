a64d08cd8594c6aeab62b76132503aae7dfa0878
Ivan Belyaev <bel3187228@yandex.ru>
2023-02-18 14:18:14 +0300

    Create main.py

---
+++ HomeWork/10_HW/main.py
@@ -0,0 +1,10 @@
+# from telegram import Update
+# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# from bot_commands import *
+
+# app = ApplicationBuilder().token("5675216545:AAFeITy9wvOumeG-Yntz5jnnnhmuvocOAJQ").build()
+
+# app.add_handler(CommandHandler("start", start))
+
+# print('server start')
+# app.run_polling()
